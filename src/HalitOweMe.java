f0f6275767c7253c8922775343cce1dbe88890da
Admin <ozank1000@gmail.com>
2022-07-08 21:38:39 -0400

    Halit owe me at the beginning of the course!

---
+++ src/HalitOweMe.java
@@ -0,0 +1,11 @@
+public class HalitOweMe {
+
+    public static void main(String[] args) {
+
+        float myMoney = 1_000_000F;
+
+        System.out.println("Halit please send me this amount of money : " + myMoney);
+
+    }
+
+}
