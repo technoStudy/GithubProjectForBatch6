81728f03e001e869b43a75934f3a5f1e5b4a599b
talalshatra@yahoo.com <talalshatra1966>
2022-07-08 21:43:11 -0400

    Wishing

--- src/TalalClass.java
+++ src/TalalClass.java
@@ -2,6 +2,6 @@
 
 public class TalalClass {
     public static void main(String[] args) {
-        System.out.println("Have a good weekend");
+        System.out.println("Have a good weekend!!");
     }
 }
